4f2f61fc20aac25c175591a44127795fe4bccbb4
Daniel Morton <daniel@djmorton.com>
2016-09-06 21:14:20 -0400

    Added ENV variable based host to production database configuration - Also modified user creation in seed to skip validation - Also modified Browser title to blog title

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 if Rails.env == 'production'
   general_category = Category.create(name: 'General')
-  User.create(email: 'user@example.com', name: 'Some User', password_hash: '$2a$10$3qyhvGbFtW6qK6B3aO/TnuopZ1XD/ZBP.iGT2GHuDRg0gjAAR/gK.')
+  User.new(email: 'user@example.com', name: 'Some User', password_hash: '$2a$10$3qyhvGbFtW6qK6B3aO/TnuopZ1XD/ZBP.iGT2GHuDRg0gjAAR/gK.').save(validate: false)
 elsif Rails.env == 'development'
   general_category = Category.create(name: 'General')
 
@@ -9,5 +9,5 @@ elsif Rails.env == 'development'
   Entry.create(title: 'This is my first blog entry', summary: 'The first blog I ever wrote', entry: 'This is a bunch of text', is_published: false, tags: '', category: general_category)
   Entry.create(title: 'This is my second blog entry', summary: 'The second blog I ever wrote', entry: 'This is a bunch of text', is_published: false, tags: '', category: general_category)
 
-  User.create(email: 'user@example.com', name: 'Some User', password_hash: '$2a$10$3qyhvGbFtW6qK6B3aO/TnuopZ1XD/ZBP.iGT2GHuDRg0gjAAR/gK.')
+  User.new(email: 'user@example.com', name: 'Some User', password_hash: '$2a$10$3qyhvGbFtW6qK6B3aO/TnuopZ1XD/ZBP.iGT2GHuDRg0gjAAR/gK.').save(validate: false)
 end
