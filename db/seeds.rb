9c0bb1c6ecdf09e6bc98e4322bed7b40987a3f6d
Daniel Morton <daniel@djmorton.com>
2016-09-06 20:26:19 -0400

    Made seeds.rb conditional on the rails environment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+if Rails.env == 'production'
+  general_category = Category.create(name: 'General')
+  User.create(email: 'user@example.com', name: 'Some User', password_hash: '$2a$10$3qyhvGbFtW6qK6B3aO/TnuopZ1XD/ZBP.iGT2GHuDRg0gjAAR/gK.')
+elsif Rails.env == 'development'
+  general_category = Category.create(name: 'General')
 
-general_category = Category.create(name: 'General')
+  Setting.create(blog_title: 'The Blog', blog_subtitle: 'A blog aboug stuff and things', display_bio: false, bio: '')
 
-Setting.create(blog_title: 'The Blog', blog_subtitle: 'A blog aboug stuff and things', display_bio: false, bio: '')
+  Entry.create(title: 'This is my first blog entry', summary: 'The first blog I ever wrote', entry: 'This is a bunch of text', is_published: false, tags: '', category: general_category)
+  Entry.create(title: 'This is my second blog entry', summary: 'The second blog I ever wrote', entry: 'This is a bunch of text', is_published: false, tags: '', category: general_category)
 
-Entry.create(title: 'This is my first blog entry', summary: 'The first blog I ever wrote', entry: 'This is a bunch of text', is_published: false, tags: '', category: general_category)
-Entry.create(title: 'This is my second blog entry', summary: 'The second blog I ever wrote', entry: 'This is a bunch of text', is_published: false, tags: '', category: general_category)
-
-User.create(email: 'user@example.com', name: 'Some User', password_hash: '$2a$10$3qyhvGbFtW6qK6B3aO/TnuopZ1XD/ZBP.iGT2GHuDRg0gjAAR/gK.')
+  User.create(email: 'user@example.com', name: 'Some User', password_hash: '$2a$10$3qyhvGbFtW6qK6B3aO/TnuopZ1XD/ZBP.iGT2GHuDRg0gjAAR/gK.')
+end
